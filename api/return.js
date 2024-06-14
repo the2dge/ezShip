3f41f8815888575210fb22027123b23ac1b1fc7d
the2dge <the2dge@users.noreply.github.com>
2024-06-14 12:15:03 +0800

    Update return.js

--- api/return.js
+++ api/return.js
@@ -1,11 +1,16 @@
 export default (req, res) => {
   const { query } = req;
+
+  console.log('Query parameters:', query); // Log the query parameters
+
   const ezShipData = {
     stName: query.stName,
     stAddr: query.stAddr,
     stCate: query.stCate,
     stCode: query.stCode,
   };
+
+  console.log('ezShipData:', ezShipData); // Log the ezShipData object
 
   const redirectUrl = `https://ez-ship.vercel.app/index.html?${new URLSearchParams(ezShipData).toString()}`;
   res.redirect(redirectUrl);
